0dadd09d2e0b0942bfeb8968d8984cb60c9047c7
FullJoker <fulljoker888@gmail.com>
2016-02-12 15:47:55 +0300

    полностью изменил логику

---
+++ ReadXML/Figures/Circle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ReadXML.Figures
+{
+    class Circle : Shape
+    {
+        protected double Diameter { get; set; }
+        public Circle(int id, string name, string color, double diameter): base(id,name,color)
+        {
+            Diameter = diameter;
+        }
+        public override double CalculateSquare()
+        {
+            double radius = (Diameter)/2;
+            return Math.Pow(radius, 2) * Math.PI;
+        }
+    }
+}
---
+++ ReadXML/Figures/Triangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ReadXML.Figures
+{
+    class Triangle : Shape
+    {
+        protected double SideA { get; set; }
+        protected double SideB { get; set; }
+        protected double SideC { get; set; }
+        public Triangle(int id, string name, string color, double sideA, double sideB, double sideC): base(id,name,color)
+        {
+            SideA = sideA;
+            SideB = sideB;
+            SideC = sideC;
+        }
+        public override double CalculateSquare()
+        {
+            double perimeter = (SideA + SideB + SideC) /2;
+            return Math.Sqrt(perimeter * (perimeter - SideA) * (perimeter - SideB) * (perimeter - SideC));
+        }
+    }
+}
